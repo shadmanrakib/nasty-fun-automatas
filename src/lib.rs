9103bcf7b84727b1329fdfc62b3b36870940c2fa
shadmanrakib <srakibnyc1@gmail.com>
2024-06-04 17:05:46 -0400

    wasm

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use wasm_bindgen::prelude::*;
 
 mod nfa;
 mod parse;
@@ -6,4 +7,21 @@ mod parse;
 #[cfg(test)]
 mod tests;
 
+#[wasm_bindgen]
+pub struct Regex {
+    nfa: nfa::NFA,
+}
 
+#[wasm_bindgen]
+impl Regex {
+    #[wasm_bindgen(constructor)]
+    pub fn new(str: String) -> Self {
+        Regex {
+            nfa: nfa::NFA::from_regex(&str),
+        }
+    }
+    #[allow(non_snake_case)]
+    pub fn isMatch(&self, input: String) -> bool {
+        self.nfa.is_match(&input)
+    }
+}
